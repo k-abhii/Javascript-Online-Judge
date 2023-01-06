2d1efaf7883679e08ecb13dafe4eb648c22c53b0
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-06 21:10:24 +0530

    unsolved pattern N

---
+++ Pattern_of_N.js
@@ -0,0 +1,47 @@
+/*
+
+Pattern of N 
+Description
+
+You are given a number stored in a variable with the name N
+
+You have to print all the numbers in the range from1 to N*N, such that there are exactly N numbers on each line
+
+For example, if the value stored inN = 3, then all the numbers in the range, from [1,9] need to be printed,
+such that there are 3 numbers on each line. Therefore, the required output is
+
+1 2 3
+4 5 6
+7 8 9
+
+Input
+The first and the only line of the input contains the value stored in the variable N
+
+
+Output
+Print all the numbers in the range from [1, N*N], as shown in the problem statement, such that there are N numbers on 
+each line
+
+4
+
+1 2 3 4 
+5 6 7 8 
+9 10 11 12 
+13 14 15 16 
+
+
+HINT 
+
+In the sample test case, the value stored atN = 4. 
+Therefore, all the values in the range from[1,16]need to be printed.
+
+Also, there should be 4 elements on each line. Therefore, the output becomes
+
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16
+
+
+
+*/
