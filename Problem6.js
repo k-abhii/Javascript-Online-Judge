7fefe52b33cabc7579ea2af1364c044135e891c7
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 21:18:23 +0530

    Problem6

---
+++ Problem6.js
@@ -0,0 +1,22 @@
+for (let father = 10; father >= 1; father--) {
+  let bag = "";
+  for (let son = 1; son <= father; son++) {
+    bag += son + " ";
+  }
+  console.log(bag);
+}
+
+/*
+
+1 2 3 4 5 6 7 8 9 10 
+1 2 3 4 5 6 7 8 9 
+1 2 3 4 5 6 7 8
+1 2 3 4 5 6 7
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+*/
