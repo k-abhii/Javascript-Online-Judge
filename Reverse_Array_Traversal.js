638d5cbeb213a29198a54469de25bb59380d68c7
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 20:37:00 +0530

    Reverse Array Traversal

---
+++ Reverse_Array_Traversal.js
@@ -0,0 +1,40 @@
+/*
+
+Reverse Array Traversal 
+Description
+
+You are given an array, stored in a variable with the name arr
+
+The size of the array is stored in a variable with the name size
+
+You have to print the reverse traversal of the array
+
+For example, consider the value stored in size = 4, and the array is arr = [1 2 3 4]. Then, the required output will be
+
+4 3 2 1
+
+Input
+The first line of the input contains the value stored in size
+
+The next line of the input contains the values stored in the array. All the values are on a single line
+separated by space
+
+
+Output
+You have to print the reverse traversal of the array, as shown in the problem statement
+
+Sample Input 1 
+5
+1 3 2 4 5
+
+Sample Output 1
+5 4 2 3 1
+
+
+In the sample test case, the value stored in size = 5, and the array is given by arr = [1 3 2 4 5]
+
+Therefore, the reverse traversal of this array will be
+
+5 4 2 3 1
+
+*/
