e5c5bb7f058a6e01d601b8214f7e78cd366395ca
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 21:34:28 +0530

    Right angled triangle with star

---
+++ Problem8.js
@@ -0,0 +1,23 @@
+for (let father = 1; father <= 10; father++) {
+  let bag = "";
+  for (let son = 1; son <= father; son++) {
+    bag += "* ";
+  }
+  console.log(bag);
+}
+
+/*
+
+
+* 
+* * 
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * * *
+* * * * * * * *
+* * * * * * * * *
+* * * * * * * * * *
+
+*/
