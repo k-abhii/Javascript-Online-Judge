c8b2397afda0ab31eecd675e2415f01406280e7e
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 22:11:40 +0530

    Break will break the inner loop when condition fulfilled

--- Problem11.js
+++ Problem11.js
@@ -1,6 +1,9 @@
 for (let i = 1; i <= 5; i++) {
   console.log("Loop Starts -----------------");
   for (j = 1; j <= 5; j++) {
+    if (j > i) {
+      break;
+    }
     console.log("i is " + i + "  j is " + j);
   }
   console.log("____________Loop Ends _______");
@@ -36,6 +39,36 @@ i is 4  j is 2
 i is 4  j is 3
 i is 4  j is 4
 i is 4  j is 5
+____________Loop Ends _______
+Loop Starts -----------------
+i is 5  j is 1
+i is 5  j is 2
+i is 5  j is 3
+i is 5  j is 4
+i is 5  j is 5
+____________Loop Ends _______
+
+*/
+
+/*
+
+Loop Starts -----------------
+i is 1  j is 1
+____________Loop Ends _______
+Loop Starts -----------------
+i is 2  j is 1
+i is 2  j is 2
+____________Loop Ends _______
+Loop Starts -----------------
+i is 3  j is 1
+i is 3  j is 2
+i is 3  j is 3
+____________Loop Ends _______
+Loop Starts -----------------
+i is 4  j is 1
+i is 4  j is 2
+i is 4  j is 3
+i is 4  j is 4
 ____________Loop Ends _______
 Loop Starts -----------------
 i is 5  j is 1
