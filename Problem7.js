1993a2aa97cd8f236bce585f81b4938683db34b4
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 21:30:30 +0530

    Star Inverted Right Angle Triangle

---
+++ Problem7.js
@@ -0,0 +1,20 @@
+for (let father = 10; father >= 1; father--) {
+  let bag = "";
+  for (let son = 1; son <= father; son++) {
+    bag += "* ";
+  }
+  console.log(bag);
+}
+
+/*
+ * * * * * * * * * *
+ * * * * * * * * *
+ * * * * * * * *
+ * * * * * * *
+ * * * * * *
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+ */
