99290c6ccfae44b3d836ccbe842c7d761c6e6d1a
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 22:05:30 +0530

    Internal and External Loop Variable

---
+++ Problem11.js
@@ -0,0 +1,48 @@
+for (let i = 1; i <= 5; i++) {
+  console.log("Loop Starts -----------------");
+  for (j = 1; j <= 5; j++) {
+    console.log("i is " + i + "  j is " + j);
+  }
+  console.log("____________Loop Ends _______");
+}
+
+/*
+
+
+Loop Starts -----------------
+i is 1  j is 1
+i is 1  j is 2
+i is 1  j is 3
+i is 1  j is 4
+i is 1  j is 5
+____________Loop Ends _______
+Loop Starts -----------------
+i is 2  j is 1
+i is 2  j is 2
+i is 2  j is 3
+i is 2  j is 4
+i is 2  j is 5
+____________Loop Ends _______
+Loop Starts -----------------
+i is 3  j is 1
+i is 3  j is 2
+i is 3  j is 3
+i is 3  j is 4
+i is 3  j is 5
+____________Loop Ends _______
+Loop Starts -----------------
+i is 4  j is 1
+i is 4  j is 2
+i is 4  j is 3
+i is 4  j is 4
+i is 4  j is 5
+____________Loop Ends _______
+Loop Starts -----------------
+i is 5  j is 1
+i is 5  j is 2
+i is 5  j is 3
+i is 5  j is 4
+i is 5  j is 5
+____________Loop Ends _______
+
+*/
