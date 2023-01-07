d00ae48d30a316cda675167ca3885da7c641bd2d
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-07 21:41:48 +0530

    Pyramid

---
+++ Problem9.js
@@ -0,0 +1,15 @@
+for (let father = 1; father <= 10; father++) {
+  let bag = "";
+  for (let son = 1; son <= father; son++) {
+    bag += "* ";
+  }
+  console.log(bag);
+}
+
+for (let f = 10; f >= 1; f--) {
+  let jhola = "";
+  for (let s = 1; s <= f; s++) {
+    jhola += "* ";
+  }
+  console.log(jhola);
+}
